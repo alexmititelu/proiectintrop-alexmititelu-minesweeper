ec2a9fdeba3bb0d6087b1dca1173b2cb085ca0ba
alexmititelu <mititelu.alex@yahoo.com>
2017-01-03 22:39:30 +0200

    Modificare la functia de creare a tabloului
    M-am gandit ca jocul sa poata fi jucat pe un tablou de forma m x n, nu
    unul de forma patratica

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 
 using namespace std;
-void creareTablou(int A[100][100],int n)
+void creareTablou(int A[100][100],int nrLinii,int nrColoane)
 {
     int i,j;
-    for(i=1;i<=n;i++)
-        for(j=1;j<=n;j++)
+    for(i=1;i<=nrLinii;i++)
+        for(j=1;j<=nrColoane;j++)
             A[i][j]=0;
-    for(j=0;j<=n+1;j++)
+    for(j=0;j<=nrColoane+1;j++)
     {
         A[0][j]=-1;
-        A[n+1][j]=-1;
+        A[nrLinii+1][j]=-1;
     }
-    for(i=1;i<=n;i++)
+    for(i=1;i<=nrLinii;i++)
     {
         A[i][0]=-1;
-        A[i][n+1]=-1;
+        A[i][nrColoane+1]=-1;
     }
+}
+
+void generareHarta(int A[100][100],int dificultate)
+{
+
 }
 int main()
 {
-    int tablou[100][100],n,i,j;
-    n=9;
-    creareTablou(tablou,n);
-    for(i=1;i<=n;i++)
+    int tablou[100][100],nrLinii,nrColoane,i,j;
+    nrLinii=9;
+    nrColoane=9;
+    creareTablou(tablou,16,9);
+    for(i=1;i<=nrLinii;i++)
         {
-            for(j=1;j<=n;j++)
+            for(j=1;j<=nrColoane;j++)
             cout<<tablou[i][j]<<" ";
             cout<<endl;
         }
