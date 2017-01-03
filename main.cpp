ae6405b16d327cfa1d7899503d5fa55fd5a1190e
alexmititelu <mititelu.alex@yahoo.com>
2017-01-03 23:48:22 +0200

    Am inceput sa lucrez cu structuri si am construit functia de generare a unei harti
    Am schimbat felul in care lucram pana acum cu tabloul astfel ca acum
    folosesc o structura specifica
    Functia de generare a unei harti functioneaza perfect si genereaza un
    numar de mine pe pozitii selectate random ((srand) + functia time) din
    tabloul nostru care reprezinta harta

--- main.cpp
+++ main.cpp
@@ -1,38 +1,58 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
-void creareTablou(int A[100][100],int nrLinii,int nrColoane)
+struct tablou
+{
+    int nrLinii,nrColoane,nrMine;
+    int valoare[100][100];
+};
+void creareTablou(tablou &A)
 {
     int i,j;
-    for(i=1;i<=nrLinii;i++)
-        for(j=1;j<=nrColoane;j++)
-            A[i][j]=0;
-    for(j=0;j<=nrColoane+1;j++)
+    for(i=1;i<=A.nrLinii;i++)
+        for(j=1;j<=A.nrColoane;j++)
+            A.valoare[i][j]=0;
+    for(j=0;j<=A.nrColoane+1;j++)
     {
-        A[0][j]=-1;
-        A[nrLinii+1][j]=-1;
+        A.valoare[0][j]=-1;
+        A.valoare[A.nrLinii+1][j]=-1;
     }
-    for(i=1;i<=nrLinii;i++)
+    for(i=1;i<=A.nrLinii;i++)
     {
-        A[i][0]=-1;
-        A[i][nrColoane+1]=-1;
+        A.valoare[i][0]=-1;
+        A.valoare[i][A.nrColoane+1]=-1;
     }
 }
 
-void generareHarta(int A[100][100],int dificultate)
+void generareHarta(tablou &A)
 {
-
+     srand(time(NULL));
+        for(int i=1;i<=A.nrMine;)
+        {
+            int x=rand()%A.nrLinii+1;
+            int y=rand()%A.nrColoane+1;
+            if(A.valoare[x][y]==0)
+            {
+                A.valoare[x][y]=9;
+                i++;
+            }
+        }
 }
 int main()
 {
-    int tablou[100][100],nrLinii,nrColoane,i,j;
-    nrLinii=9;
-    nrColoane=9;
-    creareTablou(tablou,16,9);
-    for(i=1;i<=nrLinii;i++)
+    tablou test;
+    test.nrLinii=9;
+    test.nrColoane=9;
+    test.nrMine=9;
+    creareTablou(test);
+    generareHarta(test);
+    for(int i=1;i<=test.nrLinii;i++)
         {
-            for(j=1;j<=nrColoane;j++)
-            cout<<tablou[i][j]<<" ";
+            for(int j=1;j<=test.nrColoane;j++)
+            cout<<test.valoare[i][j]<<" ";
             cout<<endl;
         }
+
+
 }
