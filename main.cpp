225032e2ac5ce834e1fd278d32bd2f91da1c3383
alexmititelu <mititelu.alex@yahoo.com>
2017-01-04 02:55:29 +0200

    Am adaugat functia care creeaza tabloul solutiei finale si am construit un tablou auxiliar care va fi si afisat la fiecare pas
    Tabloul auxiliar este de fapt ceea ce vede jucatorul, initial acesta
    fiind alcatuit doar din caractere ce simuleaza o tabla cu celule (n x
    m), toate nedescoperite (cu fata in jos). Lui ii vor fi afisate
    informatii stocate in tabloul cu solutia data daca va deschide corect
    celulele

--- main.cpp
+++ main.cpp
@@ -5,54 +5,112 @@ using namespace std;
 struct tablou
 {
     int nrLinii,nrColoane,nrMine;
-    int valoare[100][100];
+    char valoare[100][100];
+};
+struct tablouLive
+{
+    int nrLinii,nrColoane,nrMine,nrFlaguri;
+    char valoare[100][100];
 };
 void creareTablou(tablou &A)
 {
     int i,j;
-    for(i=1;i<=A.nrLinii;i++)
-        for(j=1;j<=A.nrColoane;j++)
-            A.valoare[i][j]=0;
-    for(j=0;j<=A.nrColoane+1;j++)
+    for(i=1; i<=A.nrLinii; i++)
+        for(j=1; j<=A.nrColoane; j++)
+            A.valoare[i][j]='0';
+    for(j=0; j<=A.nrColoane+1; j++)
     {
-        A.valoare[0][j]=-1;
-        A.valoare[A.nrLinii+1][j]=-1;
+        A.valoare[0][j]='!';
+        A.valoare[A.nrLinii+1][j]='!';
     }
-    for(i=1;i<=A.nrLinii;i++)
+    for(i=1; i<=A.nrLinii; i++)
     {
-        A.valoare[i][0]=-1;
-        A.valoare[i][A.nrColoane+1]=-1;
+        A.valoare[i][0]='!';
+        A.valoare[i][A.nrColoane+1]='!';
     }
 }
 
 void generareHarta(tablou &A)
 {
-     srand(time(NULL));
-        for(int i=1;i<=A.nrMine;)
+    srand(time(NULL));
+    for(int i=1; i<=A.nrMine;)
+    {
+        int x=rand()%A.nrLinii+1;
+        int y=rand()%A.nrColoane+1;
+        if(A.valoare[x][y]=='0')
         {
-            int x=rand()%A.nrLinii+1;
-            int y=rand()%A.nrColoane+1;
-            if(A.valoare[x][y]==0)
-            {
-                A.valoare[x][y]=9;
-                i++;
-            }
+            A.valoare[x][y]='*';
+            i++;
         }
+    }
 }
+int nrMineVecine(int x,int y, tablou A)
+{
+    int nr=0;
+    int v[]={-1,-1,0,1,1,1,0,-1},t[]={0,1,1,1,0,-1,-1,-1};
+    for(int i=0;i<=7;i++)
+        if(A.valoare[x+v[i]][y+t[i]]=='*')
+            nr++;
+    return nr;
+
+}
+void completareTablou(tablou &A)
+{
+    for(int i=1;i<=A.nrLinii;i++)
+        for(int j=1;j<=A.nrColoane;j++)
+            if(A.valoare[i][j]!='*')
+                A.valoare[i][j]=nrMineVecine(i,j,A)+'0';
+}
+void construireTablouLive(tablou A,tablouLive &AUX)
+{
+    AUX.nrLinii=A.nrLinii;
+    AUX.nrColoane=A.nrColoane;
+    AUX.nrMine=A.nrMine;
+    AUX.nrFlaguri=0;
+    for(int i=1; i<=AUX.nrLinii; i++)
+        for(int j=1; j<=AUX.nrColoane; j++)
+            AUX.valoare[i][j]='-';
+
+}
+void afisareTablouLive(tablouLive AUX)
+{
+    cout<<"====================="<<endl<<endl;
+    for(int i=1; i<=AUX.nrLinii; i++)
+    {
+        for(int j=1; j<=AUX.nrColoane; j++)
+            cout<<AUX.valoare[i][j];
+        cout<<endl;
+    }
+    cout<<endl<<"~~~~~~"<<"Flags:"<<AUX.nrFlaguri<<"/"<<AUX.nrMine<<endl;
+}
+
+
 int main()
 {
     tablou test;
+    tablouLive Aux;
     test.nrLinii=9;
     test.nrColoane=9;
     test.nrMine=9;
     creareTablou(test);
     generareHarta(test);
-    for(int i=1;i<=test.nrLinii;i++)
-        {
-            for(int j=1;j<=test.nrColoane;j++)
+    for(int i=1; i<=test.nrLinii; i++)
+    {
+        for(int j=1; j<=test.nrColoane; j++)
             cout<<test.valoare[i][j]<<" ";
-            cout<<endl;
-        }
+        cout<<endl;
+    }
+    cout<<"!!!!!!!!!!"<<endl;
+    construireTablouLive(test,Aux);
+    afisareTablouLive(Aux);
+    completareTablou(test);
+     for(int i=1; i<=test.nrLinii; i++)
+    {
+        for(int j=1; j<=test.nrColoane; j++)
+            cout<<test.valoare[i][j]<<" ";
+        cout<<endl;
+    }
+
 
 
 }
